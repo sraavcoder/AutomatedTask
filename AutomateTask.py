db43a59ef5bc636ee6e92d70e4e1b0a4675f4e8c
sraavcoder <61928159+sraavcoder@users.noreply.github.com>
2020-10-31 13:05:50 +0530

    Add files via upload

---
+++ AutomateTask.py
@@ -0,0 +1,36 @@
+import shutil
+import os
+
+# E:/Sravans Desktop Apps
+# C:/Users/Teju Sraav/Desktop/Backup
+
+source_path = input("Pls give the path of a Folder you want to take Backup : ")
+back_path = input(
+    "Pls give the path of a Folder where you want to store the Backup : ")
+
+listOfItems = os.listdir(source_path)
+
+for i in listOfItems:
+    FileName, Extension = os.path.splitext(i)
+    if(Extension == '' or Extension == 'pc'):
+        continue
+    path = source_path+'/'+i
+    dest_path = back_path+'/'
+    shutil.copy(path, dest_path)
+
+print("Copied The Files and Sorting them!!")
+
+listOfItems2 = os.listdir(back_path)
+
+for i in listOfItems2:
+    FileName, Extension = os.path.splitext(i)
+    if(Extension == ''):
+        continue
+
+    finalPath = back_path + '/' + Extension
+    exist = os.path.exists(finalPath)
+    if(exist == False):
+        os.mkdir(finalPath)
+    shutil.move(back_path+'/'+i, finalPath+'/'+i)
+
+print("Sorting Completed!!")
